7b63596cb2260f9042584f9a77fd170fbac6c649
Yanhong Wu <wdan1990@gmail.com>
2013-11-05 16:17:56 +0800

    Update

--- ListCycleII.java
+++ ListCycleII.java
@@ -10,18 +10,24 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
+    public ListNode detectCycle(ListNode head) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        if (head == null) return false;
+        if (head == null) return null;
         ListNode first = head;
         ListNode second = head;
-        while(second.next!=null){
+        while (second.next!=null){
             first = first.next;
-            second= second.next.next;
-            if (first == second) return true;
+            second = second.next.next;
+            if (first == second) break;
             if (second == null) break;
         }
-        return false;
+        if (second == null || second.next == null) return null;
+        first = head;
+        while (first!=second){
+            first = first.next;
+            second = second.next;
+        }
+        return first;
     }
 }
